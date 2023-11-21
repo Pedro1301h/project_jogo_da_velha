72c67d4f7fa82ed748b72189fecdc9b6082e70a2
Pedro <pedro1301@gmail.com>
2023-11-21 17:52:54 -0300

    Adicionando script

---
+++ sript.js
@@ -0,0 +1,29 @@
+"use strict"
+
+let jogaDois = document.querySelector('#jogaDois')
+let jogaRandom = document.querySelector('#jogaRandom')
+let jogo = document.querySelector('#jogo')
+let botoes = document.querySelector('#botoes')
+let quad1 = document.querySelector('#quadro1')
+let quadro
+
+function criarQuadros(){
+    for(let i = 1;i < 10;i++){
+        quadro = document.createElement('div')
+        jogo.appendChild(quadro)
+        quadro.setAttribute('class','quadros')
+        quadro.setAttribute('id','quadro'+[i])
+        quadro.innerHTML='<br>'+[i]
+    }
+    
+}
+
+jogaDois.addEventListener('click',function(){
+    botoes.innerHTML=''
+    criarQuadros()
+})
+
+jogaRandom.addEventListener('click',function(){
+    botoes.innerHTML=''
+    criarQuadros()
+})
